2726c495b2a9beb264ca841a6717e8bd256e94c1
john hidalgo <john.hidalgo@peaku.ai>
2023-03-01 21:57:40 -0500

    Creacion del menu principal

--- ConversorDeMoneda/src/ConversorMoneda/MenuPrincipal.java
+++ ConversorDeMoneda/src/ConversorMoneda/MenuPrincipal.java
@@ -4,6 +4,8 @@ import javax.swing.JOptionPane;
 
 public class MenuPrincipal {
     public static void main(String[] args){
-        JOptionPane.showMessageDialog(null, "Javadesde0.com");
+        Object [] tipoDeCambio = {"Conversor de moneda", "Conversor de temperatura"};
+        
+        Object opcion = JOptionPane.showInputDialog(null,"Selecciona una opción de conversión", "Menu",JOptionPane.QUESTION_MESSAGE,null,tipoDeCambio, tipoDeCambio[0]);
     }
 }
