d0d01991db39436b697c37cfc39eaa6e3dee5365
john hidalgo <john.hidalgo@peaku.ai>
2023-03-01 21:45:21 -0500

    Creación del menu principal

---
+++ ConversorDeMoneda/src/ConversorMoneda/MenuPrincipal.java
@@ -0,0 +1,9 @@
+package ConversorMoneda;
+
+import javax.swing.JOptionPane;
+
+public class MenuPrincipal {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Javadesde0.com");
+    }
+}
