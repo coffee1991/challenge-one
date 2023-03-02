3eeed240b454ce556c7b0139eaa353e006840f6b
coffee1991 <jhonalex1007@hotmail.com>
2023-03-02 10:04:21 -0500

    ventana del valor convertido

---
+++ ConversorDeMoneda/src/ConversorMoneda/valorConversion.java
@@ -0,0 +1,11 @@
+package ConversorMoneda;
+
+import javax.swing.JOptionPane;
+
+public class valorConversion {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "tienes ?? dinero");
+	}
+
+}
