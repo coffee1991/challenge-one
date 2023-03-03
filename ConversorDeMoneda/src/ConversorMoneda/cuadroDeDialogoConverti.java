6016e695a72b8e3628b616d56b32580c2124fca3
coffee1991 <jhonalex1007@hotmail.com>
2023-03-03 11:14:04 -0500

    se agredo el caudro de dialogo para el tipo de moneda que se quiere convertir

---
+++ ConversorDeMoneda/src/ConversorMoneda/cuadroDeDialogoConverti.java
@@ -0,0 +1,13 @@
+package ConversorMoneda;
+
+import javax.swing.JOptionPane;
+
+public class cuadroDeDialogoConverti {
+
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog(null, "Ingrese la cantidad de dinero que desea convertir:",
+                "Entrada", JOptionPane.QUESTION_MESSAGE);
+
+	}
+
+}
